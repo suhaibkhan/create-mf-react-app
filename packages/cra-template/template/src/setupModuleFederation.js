a541b7cc08d1599a3bdbef050f613d5c480623e0
Suhaib Khan <suhaibklm@gmail.com>
2023-04-23 16:54:51 +0400

    mf changes

---
+++ packages/cra-template/template/src/setupModuleFederation.js
@@ -0,0 +1,15 @@
+const deps = require('../package.json').dependencies;
+
+module.exports = {
+  name: 'mfApp',
+  library: { type: 'var', name: 'mfApp' },
+  filename: 'remoteEntry.js',
+  exposes: { '.': './src/App.js' },
+  shared: {
+    react: { singleton: true, requiredVersion: deps.react },
+    'react-dom': { singleton: true, requiredVersion: deps['react-dom'] }
+  },
+//   remotes: {
+//     mfApp2: 'mfApp2@http://localhost:3001/remoteEntry.js',
+//   },
+};
